27686ec627f164292e9729112a47229a3c0e68a9
belforz <macedobeiramar@hotmail.com>
2023-03-30 22:24:00 -0300

    cadastro agenda

--- inicio_agenda/app/src/main/java/com/example/novappli/MainActivity.java
+++ inicio_agenda/app/src/main/java/com/example/novappli/MainActivity.java
@@ -1,13 +1,32 @@
 package com.example.novappli;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+    Button btInicio;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        btInicio = findViewById(R.id.btn1);
+
+        btInicio.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                chamaSegundaTela();
+            }
+
+            });
     }
-}
+
+        private void chamaSegundaTela (){
+        Intent it = new Intent(this, Cadastro.class);
+        startActivityForResult(it, 1);
+        }
+    }
